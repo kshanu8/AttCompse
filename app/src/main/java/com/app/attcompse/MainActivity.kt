d3cff524405cb5c22fde7dd266fdc2207cf51f28
Shanu <kshanu8@gmail.com>
2024-01-11 12:44:54 +0530

    Push code to github

--- app/src/main/java/com/app/attcompse/MainActivity.kt
+++ app/src/main/java/com/app/attcompse/MainActivity.kt
@@ -80,7 +80,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AttCompseTheme {
                 // A surface container using the 'background' color from the theme
-                
+                Log.e("Local","Added")
                 MyApp(modifier = Modifier.fillMaxSize())
                 /*Surface(
                     modifier = Modifier.fillMaxSize(),
